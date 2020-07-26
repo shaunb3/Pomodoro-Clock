569e1fe680d010900a9e4cd07b98a2b3bc2cccd5
ris3n <risensx22@protonmail.com>
2020-07-26 12:55:28 +0000

    session time and break time timers working

--- src/App.js
+++ src/App.js
@@ -1,13 +1,59 @@
 import React,{Component} from 'react';
 
 import './App.css';
-
+let sessionInterval
 class App extends Component {
 
   state={
     breakLength:5,
-    sessionLength:25
+    sessionLength:25,
+    sessionTimeRemaining: 25*60,
+    sessionActive:false,
+    breakActive:false,
+    currentDisplay:"Session"
   }
+
+
+playPause=()=>{
+  
+
+
+  if(this.state.sessionActive === false){
+      
+
+      sessionInterval = setInterval(()=>{
+      
+
+      if(this.state.sessionTimeRemaining===0){
+
+        this.state.currentDisplay==="Session" ?
+          this.setState({
+            sessionTimeRemaining:this.state.breakLength*60 ,currentDisplay:"Break"})
+            :this.setState({
+            sessionTimeRemaining:this.state.sessionLength*60 ,currentDisplay:"Session"})
+      }
+      else{
+        this.setState((prevState) => {return    {sessionTimeRemaining:prevState.sessionTimeRemaining -1}})
+      }
+
+
+    },1000)
+    this.setState({sessionActive:true})
+
+   
+  }
+  else{
+    clearInterval(sessionInterval)    
+    this.setState({sessionActive:false})
+    
+
+  }
+
+  
+  
+}
+
+    
 
   handleClick=(event)=>{
     if(this.state.breakLength<1 || this.state.sessionLength<1){
@@ -35,11 +81,20 @@ class App extends Component {
     }
     
 
-
   }
 
+componentDidUpdate(prevProps,prevState) {
+  // Typical usage (don't forget to compare props):
+  if (this.state.sessionLength !== prevState.sessionLength) {
+    this.setState({sessionTimeRemaining:this.state.sessionLength*60})
+  }
+} //needs prevProps enent not using, else infinite loop
+
+
 render(){
+  
     return (
+      
     <div className="App">
       <div className="break">
         <h3 id="break-label">Break length</h3>
@@ -54,6 +109,17 @@ render(){
         <button id="session-increment"  onClick={this.handleClick}>+</button>
         <button id="session-decrement" onClick={this.handleClick} disabled={this.state.sessionLength===1 ? true: false}>-</button>
       </div>
+
+      <div className="display">
+      <h2>{this.state.currentDisplay}</h2>
+        <h3>{`${Math.floor(this.state.sessionTimeRemaining/60)}:${this.state.sessionTimeRemaining%60} `}</h3>
+        <button onClick={this.playPause} disabled={this.state.sessionActive}>Play</button>
+        <button onClick={this.playPause} disabled={!this.state.sessionActive}>Pause</button>
+      </div>
+
+
+
+
     </div>
   );
 }
